7f2c72512498c8e91b5dcf43061dd56ca7905ef2
Moon-404 <zhouzihao2008@126.com>
2023-12-18 15:39:51 +0800

    手钻→每当你突破敌人的格挡时，给予其5层易伤

---
+++ src/main/java/basicmod/relics/HandDrillEffect.java
@@ -0,0 +1,18 @@
+package basicmod.relics;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.relics.HandDrill;
+
+@SpirePatch(clz = HandDrill.class, method = "onBlockBroken")
+public class HandDrillEffect {
+    public static void Replace(HandDrill __instance, AbstractCreature m) {
+        __instance.flash();
+        AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(m, __instance));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(m, AbstractDungeon.player, new VulnerablePower(m, 5, false), 5));
+    }
+}
