1722c2ddbd140c075ffa8aa3e7a407eec318a15b
Moon-404 <zhouzihao2008@126.com>
2023-12-18 10:50:02 +0800

    增加上传脚本

---
+++ upload.bat
@@ -0,0 +1,3 @@
+SET GAME_PATH=G:\Steam\steamapps\common\SlayTheSpire
+copy %GAME_PATH%\mods\BalanceTheSpire.jar BalanceTheSpire\content\BalanceTheSpire.jar
+java -jar %GAME_PATH%\mod-uploader.jar upload -w BalanceTheSpire
