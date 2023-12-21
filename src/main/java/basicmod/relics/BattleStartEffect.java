e2b1ab2ebab4f902672ac3ce4441c10d1730bc7f
Moon-404 <zhouzihao2008@126.com>
2023-12-21 17:06:40 +0800

    冻结核心→战斗开始时，生成1个冰霜充能球；你的冰霜充能球获得2点额外集中

--- src/main/java/basicmod/relics/BattleStartEffect.java
+++ src/main/java/basicmod/relics/BattleStartEffect.java
@@ -3,25 +3,29 @@ package basicmod.relics;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.orbs.Frost;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 import basemod.BaseMod;
 import basemod.interfaces.OnStartBattleSubscriber;
 
 @SpireInitializer
-public class RingOfTheSerpentEffect implements OnStartBattleSubscriber {
-    public RingOfTheSerpentEffect() {
+public class BattleStartEffect implements OnStartBattleSubscriber {
+    public BattleStartEffect() {
         BaseMod.subscribe(this);
     }
 
     public static void initialize() {
-        new RingOfTheSerpentEffect();
+        new BattleStartEffect();
     }
 
     @Override
     public void receiveOnBattleStart(AbstractRoom var1) {
         if (AbstractDungeon.player.hasRelic("Ring of the Serpent")) {
             AbstractDungeon.actionManager.addToBottom(new DrawCardAction(AbstractDungeon.player, 1));
+        }
+        if (AbstractDungeon.player.hasRelic("FrozenCore")) {
+            AbstractDungeon.player.channelOrb(new Frost());
         }
     }
 }
--- src/main/java/basicmod/relics/FrozenCoreDescription.java
+++ src/main/java/basicmod/relics/FrozenCoreDescription.java
@@ -6,6 +6,6 @@ import com.megacrit.cardcrawl.relics.FrozenCore;
 @SpirePatch(clz = FrozenCore.class, method = "getUpdatedDescription")
 public class FrozenCoreDescription {
     public static String Replace(FrozenCore __instance) {
-        return "替换 #r破损核心 。你的 #y冰霜 充能球获得 #b2 点额外 #y集中 。";
+        return "替换 #r破损核心 。战斗开始时， #y生成 #b1 个 #y冰霜 充能球。你的 #y冰霜 充能球获得 #b2 点额外 #y集中 。";
     }
 }
