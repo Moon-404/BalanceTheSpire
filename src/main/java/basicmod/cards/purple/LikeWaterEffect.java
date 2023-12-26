983e3bf198c54d0087a8e09d8e9219747384d302
Moon-404 <zhouzihao2008@126.com>
2023-12-26 09:11:32 +0800

    Update LikeWaterEffect.java

--- src/main/java/basicmod/cards/purple/LikeWaterEffect.java
+++ src/main/java/basicmod/cards/purple/LikeWaterEffect.java
@@ -2,6 +2,7 @@ package basicmod.cards.purple;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.powers.watcher.LikeWaterPower;
@@ -10,7 +11,11 @@ import com.megacrit.cardcrawl.powers.watcher.LikeWaterPower;
 public class LikeWaterEffect {
     public static void Prefix(AbstractPower __instance) {
         if (__instance instanceof LikeWaterPower) {
-            AbstractDungeon.actionManager.addToBottom(new DrawCardAction(__instance.amount));
+            AbstractPlayer p = (AbstractPlayer)__instance.owner;
+            if (p.stance.ID.equals("Calm")) {
+                __instance.flash();
+                AbstractDungeon.actionManager.addToBottom(new DrawCardAction(__instance.amount));
+            }
         }
     }
 }
